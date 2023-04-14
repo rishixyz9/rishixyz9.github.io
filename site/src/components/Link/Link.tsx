43128cac8286ea6a7455308111936127228e452d
rishixyz9 <reach2rishabh@rocketmail.com>
2023-04-14 17:33:24 -0500

    aaaaaaa2q

--- site/src/components/Link/Link.tsx
+++ site/src/components/Link/Link.tsx
@@ -4,16 +4,26 @@ import styles from './link.module.css'
 interface Props {
     link: string;
     name: string;
+    button: boolean;
 }
 
 export default function Link(props: Props) {
 
-    return (
-        <div className={`font-semibold text-md text-white pl-4 pr-4 ${styles.link} flex flex-row`}>
-            {props.link[0] === '#' &&
-            <a className='z-10 align-middle' href={props.link}>{props.name}</a>}
-            {props.link[0] !== '#' &&
-            <a className='z-10 align-middle' href={props.link} target="_blank" rel="noreferrer">{props.name}</a>}
-        </div>
-    )
+    switch(props.button){
+        case true:
+            return (
+                <div className={`font-semibold text-md text-white pl-4 pr-4 ${styles.link} flex flex-row`}>
+                    <a className='z-10 align-middle w-full cursor-pointer'>{props.name}</a>
+                </div>
+            )
+        case false:
+            return (
+                <div className={`font-semibold text-md text-white pl-4 pr-4 ${styles.link} flex flex-row`}>
+                    {props.link[0] === '#' &&
+                    <a className='z-10 align-middle w-full' href={props.link}>{props.name}</a>}
+                    {props.link[0] !== '#' &&
+                    <a className='z-10 align-middle w-full' href={props.link} target="_blank" rel="noreferrer">{props.name}</a>}
+                </div>
+            )   
+    }
 }
--- site/src/pages/contact.tsx
+++ site/src/pages/contact.tsx
@@ -6,13 +6,12 @@ import Card from '@/components/Card/Card'
 import {p1, p2, p3, p4, p5} from '../data/exports'
 import { useState, useRef } from 'react'
 
-export default function Projects() {
+export default function Contact() {
 
     const bgparallax = useRef<any>(null)
-    const scrollref = useRef<any>(null)
-    const [scrolling, setScrolling] = useState(false)
-    const [scrollx, setScrollx] = useState(0)
-    const [clientx, setClientx] = useState(0)
+    const name = useRef<any>(null)
+    const email = useRef<any>(null)
+    const reason = useRef<any>(null)
 
     const handleMove = (e: any) => {
         if(bgparallax.current === null){return}
@@ -27,42 +26,30 @@ export default function Projects() {
         bgparallax.current.style.backgroundPosition = x;
     }
 
-    const onMouseDown = (e:any) => {
-        setClientx(e.clientX);
-        setScrolling(true);
-    };
-    
-    const onMouseUp = (e:any) => {
-        setScrolling(false)
-    };
+    const submit = () => {
+        console.log(name.current.value, email.current.value, reason.current.value)
 
-    const onMouseMove = (e:any)=> {
-        if (scrolling) {
-            const dx =   -1 * (e.clientX - clientx)
-            scrollref.current.scrollLeft = scrollref.current.clientWidth*dx + scrollx
-            setScrollx(scrollref.current.scrollLeft);
-            setClientx(e.clientX);
-        }
-    };
+    }
 
   return (
     <div className='flex flex-col w-full h-screen text-white snap-center bg-bg-main hero-bg' id="projects" onMouseMove={(e) => handleMove(e)} ref={bgparallax}>
         <div className='flex flex-col lg:flex-row sm:p-6 pr-0 self-center w-[calc(100vw-4rem)] h-full m-16 rounded  border-aqua border'>
             <div className='flex flex-col p-6 xl:ml-48 self-center m-lg:mt-auto'>
                 <div className='lg:text-9xl sm:text-8xl text-5xl'>
-                    Projects
+                    Get in contact with me
                 </div>
                 <div className='text-[#A8A8A8] font-light text-sm sm:text-lg'>
-                Cool things I built
+                    fill out the form to reach out to me!
                 </div>
             </div>
-            <div className='mt-auto mb-auto lg:mb-0 lg:mt-0 lg:ml-auto self-center flex flex-row overflow-x-auto snap-x snap-mandatory scroll-smooth flex-nowrap w-64 sm:w-96 2xl:w-[48rem] pb-2 gap-12 sm:mr-6 cursor-grab m-lg:mb-auto' 
-                onMouseDown={(e) => onMouseDown(e)} onMouseMove={(e) => onMouseMove(e)} onMouseUp={(e) => onMouseUp(e)} onMouseLeave={(e) => onMouseUp(e)} ref={scrollref}>
-                <Card data={p1}/>
-                <Card data={p2}/>
-                <Card data={p3}/>
-                <Card data={p4}/>
-                <Card data={p5}/>
+            <div className='w-[calc(10rem+15vw)] sm:w-96 self-center gap-6 flex flex-col  border-aqua border p-6 lg:ml-6 border-r-0 border-t-0 border-b-0 m-lg:mb-auto'>
+                <input className="text-black w-full rounded-sm pl-2 focus:outline-aqua outline-none transition-all ease-in-out" type="text" id="fname" name="fname" placeholder='name' ref={name}></input>
+                <input className="text-black w-full rounded-sm pl-2 focus:outline-aqua outline-none transition-all ease-in-out" type="text" id="email" name="email" placeholder='email' ref={email}></input>
+                <input className="text-black w-full rounded-sm pl-2 focus:outline-aqua outline-none transition-all ease-in-out" type="text" id="reason" name="email" placeholder='reason for reaching out' ref={reason}></input>
+                <div className='w-full h-6 bg-h-purple text-white link rounded' onClick={() => submit()}>
+                    <Link button={true} link={'/'} name={'Submit'}/>
+                </div>
+                
             </div>
         </div> 
     </div>
