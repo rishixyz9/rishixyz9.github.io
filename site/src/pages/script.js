cafdbdecb8fb4d37e21d821aab591d7ddd6ce8f5
rishixyz9 <reach2rishabh@rocketmail.com>
2023-04-13 23:04:16 -0500

    aaaaaa

--- site/src/pages/script.js
+++
@@ -1,8 +0,0 @@
-console.log('hi')
-
-$('#hero-bg').mousemove(function(e){
-    console.log('hi')
-    var amountMovedX = (e.pageX * -1 / 6);
-    var amountMovedY = (e.pageY * -1 / 6);
-    $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px');
-});
