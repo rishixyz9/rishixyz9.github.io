3716d7a0fa6ad8cbd241d7a44d0284c73369adec
rishixyz9 <reach2rishabh@rocketmail.com>
2023-06-19 09:57:54 -0500

    not that time either :/

--- site/src/pages/_app.tsx
+++ site/src/pages/_app.tsx
@@ -3,9 +3,9 @@ import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return 
+  return (
   <>
     <Component {...pageProps} />
     <Analytics  />
-  </>
+  </>)
 }
